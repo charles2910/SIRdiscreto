60a5feb5d343e18853dd7ce23a934790662ee5b1
Carlos Henrique Lima Melara <carloslimamelara@gmail.com>
2020-07-15 09:01:32 -0300

    Changed beta to alfa

--- sir.py
+++ sir.py
@@ -9,23 +9,23 @@ N = 1000
 I0, R0 = 1, 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1./10 
+# Contact rate, alfa, and mean recovery rate, gamma, (in 1/days).
+alfa, gamma = 0.2, 1./10 
 # A grid of time points (in days)
 t = np.linspace(0, 160, 160)
 
 # The SIR model differential equations.
-def deriv(y, t, N, beta, gamma):
+def deriv(y, t, N, alfa, gamma):
     S, I, R = y
-    dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N - gamma * I
+    dSdt = -alfa * S * I / N
+    dIdt = alfa * S * I / N - gamma * I
     dRdt = gamma * I
     return dSdt, dIdt, dRdt
 
 # Initial conditions vector
 y0 = S0, I0, R0
 # Integrate the SIR equations over the time grid, t.
-ret = odeint(deriv, y0, t, args=(N, beta, gamma))
+ret = odeint(deriv, y0, t, args=(N, alfa, gamma))
 S, I, R = ret.T
 
 # Plot the data on three separate curves for S(t), I(t) and R(t)
